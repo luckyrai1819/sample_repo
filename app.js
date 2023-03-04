17dfe303aa8f766a5aae4e435f1f68846e4cc529
luckyrai1819 <luckyrai1819@gmail.com>
2023-03-04 11:18:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,9 @@
+// console.log("lucky is back")
+// console.log(__dirname)
+// console.log(__filename)
+
+const os = require('os')
+const path = require('path')
+// const filename = path.join(__dirname, '/content', 'subfolder', 'test.txt');
+const filename = path.resolve(__dirname, '/content', 'subfolder', 'test.txt');
+console.log(filename);
